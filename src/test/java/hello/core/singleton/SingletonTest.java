8167b0782295c728714be85f2f87a9cbef57dbf7
Ohyaelim <npr05080@gmail.com>
2021-08-15 13:51:07 +0900

    간단하게 싱글톤 구현test

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,2 +1,41 @@
-package hello.core.singleton;public class SingletonTest {
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링이 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        Assertions.assertThat(memberService1).isNotEqualTo(memberService2);
+    }
+
+    /*
+    private라 안된다!!
+
+    public static void main(String[] args) {
+        SingletonService singletonService = new SingletonService();
+    }
+
+     */
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+        //인스턴스 비교하는 것이므로 isEqulaTo가 아닌 isSameAs!
+    }
 }
