e3165b34ab534a02d5bf2a3b883c0b3751e3040b
Ohyaelim <npr05080@gmail.com>
2021-08-15 16:50:50 +0900

    @Configuration, singleton

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,11 +15,15 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService(){
+        //1
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService(){
+        //2
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
@@ -28,6 +32,8 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
+        //3
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
