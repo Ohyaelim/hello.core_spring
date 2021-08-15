321a4471b1328ba3ddfd0bd87f7274f882053bca
Ohyaelim <npr05080@gmail.com>
2021-08-15 14:33:20 +0900

    싱글톤 방식 무상태로 만들기

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -1,14 +1,11 @@
 package hello.core.singleton;
 
 public class StatefulService {
-    private int price;
+    //무상태로 변환 가즈아
 
-    public void order(String name, int price){
+    public int order(String name, int price){
         System.out.println("name = " + name + " price = " + price);
-        this.price=price;//error날것
-    }
-
-    public int getPrice(){
         return price;
     }
+
 }
