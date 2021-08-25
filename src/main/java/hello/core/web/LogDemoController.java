ca8a59b3eedf94d9bd5add2c686a1dcadbce37f7
Ohyaelim <npr05080@gmail.com>
2021-08-25 09:47:28 +0900

    scope 와 provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,16 +15,19 @@ public class LogDemoController {
 
     private final LogDemoService logDemoService;
     // request scope
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURI().toString();
+        // 여기서 mylogger를 받는다
+        MyLogger myLogger = myLoggerProvider.getObject();
         // log 남을 때 [http://localhost:8080/어쩌구] 남기려고고
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
